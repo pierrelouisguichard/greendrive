ca6f7743a8f22874256b52e7188265b22f8f7198
UserPierreG <pierrelouisguichard@gmail.com>
2023-04-01 21:29:02 +0100

    multipages

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,37 +1,38 @@
 import React from 'react';
 
 function NavBar() {
+  const navStyle = {
+    letterSpacing: '2px',
+    fontWeight: '700',
+    textTransform: 'uppercase',
+    fontSize: '2em',
+    position: 'absolute',
+    top: '7%',
+    right: '5%',
+    display: 'flex',
+    justifyContent: 'center',
+    listStyle: 'none',
+    marginLeft: '35px',
+  };
 
-    const navStyle = {
-        letterSpacing: '2px',
-        fontWeight: '700',
-        textTransform: 'uppercase',
-        fontSize: '2em',
-        position: 'absolute',
-        top: '7%',
-        right: '5%',
-        display: 'flex',
-        justifyContent: 'center',
-        listStyle: 'none',
-        marginLeft: '35px'
-      }
-      
-      const buttonStyle = {
-        textDecoration: 'none',
-        background: 'white',
-        padding: '10px',
-        color: '#20782f',
-        borderRadius: '30px'
-      }
-      
+  const buttonStyle = {
+    textDecoration: 'none',
+    background: 'white',
+    padding: '10px',
+    color: '#20782f',
+    borderRadius: '30px',
+  };
 
   return (
     <nav>
       <ul style={navStyle}>
-        <li style={buttonStyle}><a href="#">Home</a></li>
-        <li style={buttonStyle}><a href="#">About</a></li>
-        <li style={buttonStyle}><a href="#">Services</a></li>
-        <li style={buttonStyle}><a href="#">Contact</a></li>
+        <li>
+          <a style={buttonStyle} href='/forum'>Forum</a>
+        </li>
+        <li>
+          <a style={buttonStyle} href='/news'>News</a>
+        </li>
+        <li style={buttonStyle}>Button 3</li>
       </ul>
     </nav>
   );
