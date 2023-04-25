60af0b81aca7c19d72dc595bc7abcec5b950af01
UserPierreG <pierrelouisguichard@gmail.com>
2023-04-25 21:50:06 +0100

    News_Done

--- src/Slider.jsx
+++ src/Slider.jsx
@@ -1,38 +1,60 @@
 import Carousel from "react-bootstrap/Carousel";
-import img3 from "../assets/3-Ground.png";
-import img4 from "../assets/2-Ground.png";
-import img5 from "../assets/1-Ground.png";
+import img3 from "../assets/news1.jpg";
+import img4 from "../assets/news2.jpeg";
+import img5 from "../assets/news3.jpg";
+import Button from "react-bootstrap/Button";
 
 function Slider() {
   return (
-    <Carousel>
-      <Carousel.Item>
-        <img className="d-block w-100" src={img3} alt="First slide" />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img className="d-block w-100" src={img4} alt="Second slide" />
-
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img className="d-block w-100" src={img5} alt="Third slide" />
-
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
+    <Carousel style={style}>
+      <Carousel.Item style={{ ...itemStyle, backgroundImage: `url(${img3})` }}>
+        <Carousel.Caption style={caption}>
+          <h1>The electric car revolution</h1>
           <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            The government must do its bit to make new electric vehicles more
+            affordable if mass adoption is to be achieved on the road to net
+            zero
           </p>
+          <Button variant="secondary">Read more</Button>{" "}
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item style={{ ...itemStyle, backgroundImage: `url(${img4})` }}>
+        <Carousel.Caption style={caption}>
+          <h1>Electric cars coming soon</h1>
+          <p>
+            There are LOTS of electric vehicles that are due to come to market
+            in the near future.
+          </p>
+          <Button variant="secondary">Read more</Button>{" "}
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item style={{ ...itemStyle, backgroundImage: `url(${img5})` }}>
+        <Carousel.Caption style={caption}>
+          <h1>Powering ahead</h1>
+          <p>Six new ways to charge an electric car</p>
+          <Button variant="secondary">Read more</Button>{" "}
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
   );
 }
+
+const style = {
+  height: "100vh",
+  fontFamily: "Bahnschrift, sans-serif",
+  color: "white",
+  fontSize: "20px",
+};
+
+const itemStyle = {
+  height: "100vh",
+  backgroundRepeat: "repeat-x",
+  backgroundSize: "auto 100%",
+  backgroundPosition: "center",
+};
+
+const caption = {
+  top: "75%",
+};
 
 export default Slider;
